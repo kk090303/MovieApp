980b203bfe65369bfb534e03cd2ec54799258f4c
Kimhyundo <kk090303@naver.com>
2020-09-25 17:17:31 +0900

    GridCards

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,56 +1,89 @@
-import React, {useEffect, useState} from 'react'
-import { FaCode } from "react-icons/fa";
-import { API_URL, API_KEY,IMAGE_BASE_URL } from '../../Config';
+
 //config에 저장해 놓은 API URL과 API KEY를 사용하여 URL을 연결할 떄 편하게 사용
-import MainImage from './Sections/Mainimage';
+import React, { useEffect, useState } from 'react'
+import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
+import MainImage from './Sections/MainImage';
 import axios from 'axios';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
 
 function LandingPage() {
 
-	const [Movies, setMovies] = useState([])
+
+    const [Movies, setMovies] = useState([])
     const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [CurrentPage, setCurrentPage] = useState(0)
 
-	useEffect(()=>{
-		 const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        fetchMovies(endpoint)
 
-		fetch(endpoint)
-		.then(response => response.json())
-		.then(response => {
-
-			console.log(response)
-		
-			setMovies([...response.results])
-			setMainMovieImage(response.results[0])
-			})
-
-	},[])
+    }, [])
 
 
+    const fetchMovies = (endpoint) => {
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+                setMovies([...Movies, ...response.results])
+                setMainMovieImage(response.results[0])
+                setCurrentPage(response.page)
+            })
+    }
+
+    const loadMoreItems = () => {
+
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`;
+        fetchMovies(endpoint)
+
+    }
 
 
     return (
-        <>
-           <div style={{width : '100%', margin : '1rem auto'}}>
-				{/*Main Image*/}
+        <div style={{ width: '100%', margin: '0' }}>
 
-				{MainMovieImage &&
-				<MainImage
-				 image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
-                 title={MainMovieImage.original_title}
-                 text={MainMovieImage.overview}
-				 />
-				 }
-				<h2>Movie by Latest</h2>
-				<hr />
+            {/* Main Image */}
+            {MainMovieImage &&
+                <MainImage
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}
+                />
+            }
 
-				{/*Movie Grid Cards*/}
 
-				</div>
+            <div style={{ width: '85%', margin: '1rem auto' }}>
 
-				<div style={{display : 'flex',justifyContent: 'center'}}>
-					<button> Load more</button>
-				</div>
-        </>
+                <h2>Movies by latest</h2>
+                <hr />
+
+                {/* Movie Grid Cards */}
+
+                <Row gutter={[16, 16]} >
+
+                    {Movies && Movies.map((movie, index) => (
+                        <React.Fragment key={index}>
+                            <GridCards
+                                landingPage
+                                image={movie.poster_path ?
+                                    `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+                            />
+                        </React.Fragment>
+                    ))}
+
+                </Row>
+
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button onClick={loadMoreItems}> Load More</button>
+            </div>
+
+        </div>
     )
 }
 
