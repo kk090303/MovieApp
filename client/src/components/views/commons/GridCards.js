656885c7c3988ecd2054ee2fa824b1bf7400d2e4
Kimhyundo <kk090303@naver.com>
2020-09-29 02:19:34 +0900

    GridCards

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -1,13 +1,16 @@
 // JavaScript source code
+
+//다른 파일에서도 재사용할 수 있기 때문에 commons라는 폴더에 생성
 import React from 'react'
 import { Col } from 'antd';
+
 
 
 function GridCards(props) {
 
     if (props.landingPage) {
         return (
-            <Col lg={6} md={8} xs={24}>
+            <Col lg={6} md={8} xs={24}>		//사이즈에 따라 지정
                 <div style={{ position: 'relative' }}>
                     <a href={`/movie/${props.movieId}`} >
                         <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.movieName} />
